ac2ed05af71684d6dc03333a39fa1ea6b434403c
SanjivaniYadav <yadavsanjivani99@gmail.com>
2024-06-06 17:10:57 +0530

    Implemented create podcast page

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://polished-moth-61.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
