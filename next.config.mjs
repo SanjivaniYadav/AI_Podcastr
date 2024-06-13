1945e9ea39d27a98d431559d0f34c08a65cb919f
SanjivaniYadav <yadavsanjivani99@gmail.com>
2024-06-13 16:55:21 +0530

    Implemented Podcast Details Page

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "sensible-ladybug-306.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
